0730d8b66db76a00fb866241c4c2001b99df14d6
Taofik Hidayat <146046695+evika7@users.noreply.github.com>
2024-10-24 20:17:25 +0700

    Add files via upload

---
+++ js/sended msg.js
@@ -0,0 +1,19 @@
+const contactForm = Document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const url = e.target.action;
+    const formData = new FormData(contactForm);
+
+    fetch(url, {
+        method: "POST",
+        body: formData,
+        mode: "no-cors",
+    })
+    .then(()=>{
+        // url thank you
+        window.location.href = "thanks.html";
+    })
+    .catch((e) => alert("Error occured"));
+})
